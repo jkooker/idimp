7e97cba96e7b8e7b21ae6cf2dbce740e25d6608f
meeshmusic <meeshmusic@c9110c64-9c02-11dd-8bdc-dd70ed116f32>
2008-12-03 07:00:26 +0000

    Added javadoc-style comments for Doxygen to AudioEngine and its public methods.

--- trunk/Audio/AudioEngine.h
+++ trunk/Audio/AudioEngine.h
@@ -17,26 +17,122 @@
 #import "Wavefile.h"
 #import "Synth.h"
 
+/** AudioEngine class.
+ * The AudioEngine class controls all audio reocrding and playback.
+ * It follows the singleton pattern.
+ */
 class AudioEngine
 {
 public:
 
+   /** 
+    * AudioEngine destructor
+    */
     ~AudioEngine();    
-    static AudioEngine* getInstance();    
+    
+   /**
+    * @return A pointer to the singleton instance of AudioEngine.
+    */
+    static AudioEngine* getInstance();  
+    
+   /**
+    * Add a recording effect to this AudioEngine.
+    * @param e A pointer to the AudioEffect to be added.
+    * @see removeRecordingEffect
+    */
     void addRecordingEffect(AudioEffect* e);    
+   
+   /** 
+    * Remove a recording effect from this AudioEngine.
+    * @param e A pointer to the AudioEffect to be removed.
+    * @return true if the requested AudioEffect was found and removed, false otherwise.
+    * @see addRecordingEffect
+    */
     bool removeRecordingEffect(AudioEffect* e);    
+    
+   /**
+    * Add a synthesis effect to this AudioEngine.
+    * @param e A pointer to the AudioEffect to be added.
+    * @see removeSynthesisEffect
+    */
     void addSynthesisEffect(AudioEffect* e);    
+    
+   /** 
+    * Remove a synthesis effect from this AudioEngine.
+    * @param e A pointer to the AudioEffect to be removed.
+    * @return true if the requested AudioEffect was found and removed, false otherwise.
+    * @see addSynthesisEffect
+    */
     bool removeSynthesisEffect(AudioEffect* e);
     
+   /** 
+    * Get a pointer to the TouchSynth object associated with this AudioEngine.
+    * @return A pointer to the TouchSynth object associated with this AudioEngine.
+    */
     TouchSynth* getSynth() { return &m_synth; }
+    
+   /**
+    * Find out whether or not recording input is muted.
+    * @return true if recording input is muted, false otherwise.
+    * @see setMuteRecording
+    */
     bool getMuteRecording() { return m_recordingIsMuted; }
+    
+   /**
+    * Enable or disable muting of recorded input.
+    * @param on true to mute recording, false to unmute.
+    * @see getMuteRecording
+    */
     void setMuteRecording(bool on) { m_recordingIsMuted = on; }
+    
+   /**
+    * Find out whether or not synthesized audio is muted.
+    * @return true if synthesized audio is muted, false otherwise.
+    * @see setMuteSynth
+    */
     bool getMuteSynth() { return m_synthIsMuted; }
+    
+   /**
+    * Enable or disable muting of synthesized audio.
+    * @param true to mute synthesized audio, false to unmute.
+    * @see getMuteSynth
+    */
     void setMuteSynth(bool on) { m_synthIsMuted = on; }
+    
+   /**
+    * Find out whether audio playback and recording have been started.
+    * @return true if started, false otherwise.
+    * @see start
+    * @see stop
+    */
     bool isStarted() { return m_isStarted; }
     
+   /**
+    * Start audio playback and recording.
+    * @see stop
+    * @see isStarted
+    */
     void start();
+    
+   /**
+    * Stop audio playback and recording.
+    * @see start
+    * @see isStarted
+    */
     void stop();
+    
+   /**
+    * recordingCallback is the method called when there is recorded input available from the microphone.
+    * Do not call this method directly.  Instead, to start/stop callbacks, use start() and stop().
+    * @param inRefcon a reference to the AudioEngine instance that will process this callback.
+    * @param ioActionFlags flags describing how an AudioUnit will render the recorded data.
+    * @param inTimeStamp the time stamp of the recorded audio data.
+    * @param inBusNumber the AudioUnit bus number for recording audio.
+    * @param inNumberFrames the number of frames of recorded audio data available (a frame is one sample of all channels)
+    * @param ioData not used at this time but a necessary part of the callback function declaration
+    * @see start
+    * @see stop
+    */
     static OSStatus recordingCallback(void *inRefCon, 
                                       AudioUnitRenderActionFlags *ioActionFlags, 
                                       const AudioTimeStamp *inTimeStamp, 
@@ -44,6 +140,18 @@ public:
                                       UInt32 inNumberFrames, 
                                       AudioBufferList *ioData);
                                           
+   /**
+    * playbackCallback is the method called when the audio output device wants to request audio samples for playback
+    * Do not call this method directly.  Instead, to start/stop callbacks, use start() and stop().
+    * @param inRefcon a reference to the AudioEngine instance that will process this callback.
+    * @param ioActionFlags flags describing how an AudioUnit will render the audio data.
+    * @param inTimeStamp the time stamp of the audio data.
+    * @param inBusNumber the AudioUnit bus number for playing audio.
+    * @param inNumberFrames the number of frames of audio data requested (a frame is one sample of all channels)
+    * @param ioData a pointer to the AudioBufferList whose buffers will be filled with audio data for playback
+    * @see start
+    * @see stop
+    */
     static OSStatus playbackCallback(void *inRefCon, 
                                      AudioUnitRenderActionFlags *ioActionFlags, 
                                      const AudioTimeStamp *inTimeStamp, 
