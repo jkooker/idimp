bab567992ca576e423ed7bf5da5b1a30c01d1238
meeshmusic <meeshmusic@c9110c64-9c02-11dd-8bdc-dd70ed116f32>
2008-12-11 01:02:40 +0000

    I was away from the internet for a couple of hours so this is a big check-in:
    -removed amplitude slider from AudioButtonTest page since it now lives on the MasterAudioControls page
    
    -made MasterAudioControls page the default view when switching to the flip side the first time
    
    -starting to centralize some things in iDiMPAppDelegate
    
    -added non-editable text fields on MasterAudioControls page to show actual slider values and wired them up

--- trunk/Audio/AudioButtonTestViewController.h
+++ trunk/Audio/AudioButtonTestViewController.h
@@ -13,26 +13,19 @@
 
     IBOutlet UISlider* _ringModFreqSlider;
     IBOutlet UITextField* _ringModFreqTextField;
-    IBOutlet UISlider* _ampSlider;
-    IBOutlet UITextField* _ampTextField;
     IBOutlet UISegmentedControl* _waveformSelector;
     AudioEngine* _audioEngine;
     AudioEffectParameter* _ringModFreqParam;
-    AudioEffectParameter* _ampParam;
     AudioEffect* _ringModEffect;
-    AudioEffect* _ampEffect;
 }
 
 @property (nonatomic, retain) IBOutlet UISlider* _ringModFreqSlider;
 @property (nonatomic, retain) IBOutlet UITextField* _ringModFreqTextField;
-@property (nonatomic, retain) IBOutlet UISlider* _ampSlider;
-@property (nonatomic, retain) IBOutlet UITextField* _ampTextField;
 @property (nonatomic, retain) IBOutlet UISegmentedControl* _waveformSelector;
 
 - (void) myInit;
 - (void) dealloc;
 - (IBAction) ringModFreqSliderChanged: (id) sender;
-- (IBAction) ampSliderChanged: (id) sender;
 - (IBAction) waveformSelected: (id) sender;
 - (void) accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration;
 
--- trunk/Audio/MasterAudioControlsViewController.mm
+++ trunk/Audio/MasterAudioControlsViewController.mm
@@ -12,26 +12,45 @@
 
 @synthesize _masterAudioLevelSlider;
 @synthesize _masterAudioMuteSwitch;
+@synthesize _masterAudioLevelTextField;
 @synthesize _synthAudioLevelSlider;
 @synthesize _synthAudioMuteSwitch;
+@synthesize _synthAudioLevelTextField;
 @synthesize _recordedAudioLevelSlider;
 @synthesize _recordedAudioMuteSwitch;
+@synthesize _recordedAudioLevelTextField;
 @synthesize _networkAudioLevelSlider;
 @synthesize _networkAudioMuteSwitch;
+@synthesize _networkAudioLevelTextField;
 
 // Implement viewDidLoad to do additional setup after loading the view.
 - (void)viewDidLoad 
 {
     NSLog(@"MasterAudioControlsViewController viewDidLoad");
-        
-    // init audio engine
+
+    // store app delegate for future use
+    _appDelegate = (iDiMPAppDelegate *)[UIApplication sharedApplication].delegate;
+
+    // get instance of audio engine
     _audioEngine = AudioEngine::getInstance();
 
     // init switch states
-    [_recordedAudioMuteSwitch setOn:!_audioEngine->getMuteRecording()];
-    [_synthAudioMuteSwitch setOn:!_audioEngine->getMuteSynth()];
-    [_networkAudioMuteSwitch setOn:!_audioEngine->getMuteNetwork()];
-    [_masterAudioMuteSwitch setOn:_audioEngine->isStarted()];
+    [_recordedAudioMuteSwitch setOn: !_audioEngine->getMuteRecording()];
+    [_synthAudioMuteSwitch setOn: !_audioEngine->getMuteSynth()];
+    [_networkAudioMuteSwitch setOn: !_audioEngine->getMuteNetwork()];
+    [_masterAudioMuteSwitch setOn: _audioEngine->isStarted()];
+    
+    // enable/disable sliders
+    [_masterAudioLevelSlider setEnabled: _audioEngine->isStarted()];
+    [_recordedAudioLevelSlider setEnabled: !_audioEngine->getMuteRecording()];
+    [_synthAudioLevelSlider setEnabled: !_audioEngine->getMuteSynth()];
+    [_networkAudioLevelSlider setEnabled: !_audioEngine->getMuteNetwork()];
+    
+    // init text fields
+    [_masterAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_masterAudioLevelSlider value]]];
+    [_recordedAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_recordedAudioLevelSlider value]]];
+    [_synthAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_synthAudioLevelSlider value]]];
+    [_networkAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_networkAudioLevelSlider value]]];
     
     [super viewDidLoad];
 }
@@ -39,6 +58,9 @@
 - (IBAction) masterAudioLevelSliderChanged: (id) sender
 {
     NSLog(@"MasterAudioControlsViewController masterAudioLevelSliderChanged");
+    
+    [_appDelegate masterAudioAmpEffect]->getParameter(0)->setValue([_masterAudioLevelSlider value]);
+    [_masterAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_masterAudioLevelSlider value]]];
 }
 
 - (IBAction) masterAudioMuteSwitchChanged: (id) sender
@@ -47,11 +69,13 @@
     if (isOn)
     {
         NSLog(@"masterAudioMuteSwitchChanged starting all audio");
+        [_masterAudioLevelSlider setEnabled:true];
         _audioEngine->start();
     }
     else
     {
         NSLog(@"masterAudioMuteSwitchChanged stopping all audio");
+        [_masterAudioLevelSlider setEnabled:false];
         _audioEngine->stop();
     }
 }
@@ -59,6 +83,9 @@
 - (IBAction) synthAudioLevelSliderChanged: (id) sender
 {
     NSLog(@"MasterAudioControlsViewController synthAudioLevelSliderChanged");
+    
+    [_appDelegate synthAudioAmpEffect]->getParameter(0)->setValue([_synthAudioLevelSlider value]);
+    [_synthAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_synthAudioLevelSlider value]]];
 }
 
 - (IBAction) synthAudioMuteSwitchChanged: (id) sender
@@ -67,11 +94,13 @@
     if (isOn)
     {
         NSLog(@"synthAudioMuteSwitchChanged unmuting synth");
+        [_synthAudioLevelSlider setEnabled:true];
         _audioEngine->setMuteSynth(false);
     }
     else
     {
         NSLog(@"synthAudioMuteSwitchChanged muting synth");
+        [_synthAudioLevelSlider setEnabled:false];
         _audioEngine->setMuteSynth(true);
     }
 }
@@ -79,6 +108,9 @@
 - (IBAction) recordedAudioLevelSliderChanged: (id) sender
 {
     NSLog(@"MasterAudioControlsViewController recordedAudioLevelSliderChanged");
+    
+    [_appDelegate recordingAudioAmpEffect]->getParameter(0)->setValue([_recordedAudioLevelSlider value]);
+    [_recordedAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_recordedAudioLevelSlider value]]];
 }
 
 - (IBAction) recordedAudioMuteSwitchChanged: (id) sender
@@ -87,11 +119,13 @@
     if (isOn)
     {
         NSLog(@"recordedAudioMuteSwitchChanged unmuting recording");
+        [_recordedAudioLevelSlider setEnabled:true];
         _audioEngine->setMuteRecording(false);
     }
     else
     {
         NSLog(@"recordedAudioMuteSwitchChanged muting recording");
+        [_recordedAudioLevelSlider setEnabled:false];
         _audioEngine->setMuteRecording(true);
     }
 }
@@ -99,6 +133,9 @@
 - (IBAction) networkAudioLevelSliderChanged: (id) sender
 {
     NSLog(@"MasterAudioControlsViewController networkAudioLevelSliderChanged");
+    
+    [_appDelegate networkAudioAmpEffect]->getParameter(0)->setValue([_networkAudioLevelSlider value]);
+    [_networkAudioLevelTextField setText:[NSString stringWithFormat:@"%f", [_networkAudioLevelSlider value]]];
 }
 
 - (IBAction) networkAudioMuteSwitchChanged: (id) sender
@@ -107,11 +144,13 @@
     if (isOn)
     {
         NSLog(@"networkAudioMuteSwitchChanged unmuting network");
+        [_networkAudioLevelSlider setEnabled:true];
         _audioEngine->setMuteNetwork(false);
     }
     else
     {
         NSLog(@"networkAudioMuteSwitchChanged muting network");
+        [_networkAudioLevelSlider setEnabled:false];
         _audioEngine->setMuteNetwork(true);
     }
 }
