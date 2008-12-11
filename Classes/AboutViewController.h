c5f843a2e1f6114e4f95843f61004d3171af2240
jkooker <jkooker@c9110c64-9c02-11dd-8bdc-dd70ed116f32>
2008-12-11 16:00:14 +0000

    Add a very basic About page, which is absolutely up for revision.

---
+++ Classes/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  iDiMP
+//
+//  Created by John Kooker on 12/11/08.
+//  Copyright 2008 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AboutViewController : UIViewController {
+
+}
+
+@end
---
+++ Classes/AboutViewController.m
@@ -0,0 +1,57 @@
+//
+//  AboutViewController.m
+//  iDiMP
+//
+//  Created by John Kooker on 12/11/08.
+//  Copyright 2008 Apple Inc. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+
+@implementation AboutViewController
+
+/*
+// The designated initializer. Override to perform setup that is required before the view is loaded.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor clearColor];
+}
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
