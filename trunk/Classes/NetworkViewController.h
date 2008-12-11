af9e94e0369c8ce2b9487da442bec2e9228b8255
jkooker <jkooker@c9110c64-9c02-11dd-8bdc-dd70ed116f32>
2008-12-11 00:39:38 +0000

    Add an empty tableview for the NetworkView.

---
+++ trunk/Classes/NetworkViewController.h
@@ -0,0 +1,16 @@
+//
+//  NetworkViewController.h
+//  iDiMP
+//
+//  Created by John Kooker on 12/10/08.
+//  Copyright 2008 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NetworkViewController : UIViewController {
+
+}
+
+@end
